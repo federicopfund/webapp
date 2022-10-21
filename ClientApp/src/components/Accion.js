8fc6d0293c82251e496be6368983307ce2baaaca
federico pfund <federicopfund@gmail.com>
2022-10-20 22:07:09 -0300

    Trigger Asysncronicos para Componentes

---
+++ ClientApp/src/components/Accion.js
@@ -0,0 +1,34 @@
+
+export class Actions {
+    constructor() { }
+
+    /**
+     * Función para llamar cuando se activa el botón de apertura
+     */
+    async open() { }
+
+    /**
+     *Función para llamar cuando se activa el botón Guardar
+     */
+    async save() { }
+
+    /**
+     *Función para llamar cuando se activa el botón duplicar/guardar como
+     */
+    async saveAs() { }
+
+    /**
+     * Restablecer el editor y el controlador de archivos
+     */
+    async reset() { }
+
+    /**
+     * Función para llamar cuando se activa el botón de vista previa
+     */
+    async preview() { }
+
+    /**
+     * Función para llamar cuando se activa el botón de enfoque
+     */
+    async focus() { }
+}
