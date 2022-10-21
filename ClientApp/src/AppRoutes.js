9704e8c5c81b9e52433210abd50ab656af2da8cf
federico pfund <federicopfund@gmail.com>
2022-10-20 22:08:14 -0300

    Ruteo de componentes Nuevos

--- ClientApp/src/AppRoutes.js
+++ ClientApp/src/AppRoutes.js
@@ -1,11 +1,21 @@
+import { About } from "./components/About";
 import { Counter } from "./components/Counter";
 import { FetchData } from "./components/FetchData";
 import { Home } from "./components/Home";
+import { Service } from "./components/Service";
 
 const AppRoutes = [
   {
     index: true,
     element: <Home />
+  },
+  {
+    path: '/service',
+    element: <Service />
+  },
+  {
+    path: '/about',
+    element: <About />
   },
   {
     path: '/counter',
